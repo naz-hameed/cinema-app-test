acd0ffa7bfb36d539ba54f6b7640283af2e18226
stephschumacher <stephanie.schumacher@sky.com>
2020-04-24 08:57:14 +0100

    signup page formatting

--- src/Components/SignUpPage.jsx
+++ src/Components/SignUpPage.jsx
@@ -4,13 +4,13 @@ import SignUpForm from './SignUpForm';
 
 const SignUpPage  = () => {
   
-        const [title,setTitle] = useState('not given')
-        const [fname,setFname] = useState('not given')
-        const [lname,setLname] = useState('not given')
-        const [email,setEmail] = useState('not given')
-        const [date,setDate] = useState('not given')
-        const [phone,setPhone] = useState('not given')
-        const [gender,setGender] = useState('not given')
+        const [title,setTitle] = useState('')
+        const [fname,setFname] = useState('')
+        const [lname,setLname] = useState('')
+        const [email,setEmail] = useState('')
+        const [date,setDate] = useState('')
+        const [phone,setPhone] = useState('')
+        const [gender,setGender] = useState('')
         
       
         const showTitle = title => {
@@ -46,15 +46,18 @@ const SignUpPage  = () => {
       <div class='form'>
         <h1>Sign Up</h1>
         <p> </p>
-        <h3>Title        : {title}</h3>
-        <h3>First Name   : {fname}</h3>
-        <h3>Last Name    : {lname}</h3>
-        <h3>Email        : {email}</h3>
-        <h3>Date of Birth: {date}</h3>
-        <h3>Phone        : {phone}</h3>
-        <h3>Gender       : {gender}</h3>
+        <div className='signuparea'>
+          <h3>Title        : {title}</h3>
+          <h3>First Name   : {fname}</h3>
+          <h3>Last Name    : {lname}</h3>
+          <h3>Email        : {email}</h3>
+          <h3>Date of Birth: {date}</h3>
+          <h3>Phone        : {phone}</h3>
+          <h3>Gender       : {gender}</h3>        
+        </div> 
         <SignUpForm submitTitle={showTitle} submitLname = {showLname} submitFname = {showFname} submitDate = {showDate} submitEmail={showEmail} submitPhone={showPhone} submitGender={showGender} />
-      </div>    
+ 
+      </div>  
 )};
 
 export default SignUpPage;
